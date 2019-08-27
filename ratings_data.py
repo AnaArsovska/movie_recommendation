dc9efb5241724193b8a9336e2d05242c34f6c3e6
Ana Arsovska <anamarsovska@gmail.com>
2019-08-27 18:43:32 -0400

    separated file w data variables from models

--- ratings_data.py
+++ ratings_data.py
@@ -22,13 +22,3 @@ ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='
 #converting data to sframe objects
 train_data = graphlab.SFrame(ratings_base)
 test_data = graphlab.SFrame(ratings_test)
-
-#makes popularity model using training data
-popularity_model = graphlab.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating')
-
-#check recommended movies for a sample 5 users
-#popularity_recomm = popularity_model.recommend(users=range(1,6),k=5)
-#popularity_recomm.print_rows(num_rows=25)
-
-#check whether the recommended movies are the highest rated ones
-#ratings_base.groupby(by='movie_id')['rating'].mean().sort_values(ascending=False).head(20)
